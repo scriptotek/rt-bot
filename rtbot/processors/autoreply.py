b3c324432c59376a485881715361423b58978a01
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-04-25 16:59:16 +0200

    Add comment when resolving auto-replies

--- rtbot/processors/autoreply.py
+++ rtbot/processors/autoreply.py
@@ -24,10 +24,13 @@ class ResolveAutoReplies(Processor):
     ]
 
     def process_ticket(self, ticket):
-        match = re.search('automatic reply', ticket['Subject'], re.I)
+        match = re.search('(automatic reply|automatisk svar)', ticket['Subject'], re.I)
         if match is not None:
             log.info('[#%s] Auto-resolving ticket with subject: %s',
                      ticket['id'], ticket['Subject'])
+            if not self.rt.comment(ticket['id'], text='🏝 Saken ble automatisk lukket av rt-bot som autosvar. Spørsmål om scriptet? Kontakt Dan Michael.'):
+                log.error('[#%s] Failed to add comment to ticket!', ticket['id'])
+                return False
             if not self.rt.edit_ticket(ticket['id'], Status='resolved'):
                 log.error('[#%s] Failed to auto-resolve ticket!', ticket['id'])
                 return False

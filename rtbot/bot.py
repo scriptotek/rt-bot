40c6aa46dec70607c62809978081a1126c1b3230
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-11-23 18:32:26 +0100

    Retry on errors during processor.get_tickets() too
    The RT server can disconnect (rt.ConnectionError) during
    processor.get_tickets(), so we should catch that and retry.

--- rtbot/bot.py
+++ rtbot/bot.py
@@ -5,6 +5,7 @@ import logging
 import logging.config
 import requests
 import rt
+from functools import partial
 from dotenv import load_dotenv
 from .alma import Alma
 from .rt import Tracker
@@ -18,32 +19,41 @@ log = logging.getLogger(__name__)
 load_dotenv()
 
 
+def retry_on_error(do_stuff):
+    while True:
+        try:
+            do_stuff()
+            break
+        except requests.RequestException as ex:
+            log.warning('Got requestion exception: %s, will retry in a sec.', ex)
+            time.sleep(3)
+            # retry
+            pass
+        except rt.UnexpectedResponse as ex:
+            log.warning('Got unexpected response from RT: %s, will retry in a sec.', ex)
+            time.sleep(3)
+            # retry
+            pass
+        except rt.ConnectionError as ex:
+            log.warning('Connection error while processing: %s, will retry in a sec.', ex)
+            time.sleep(3)
+            # retry
+            pass
+        time.sleep(1)
+
+
+def process_tickets(processor):
+    for ticket in processor.get_tickets():
+        time.sleep(1)
+        retry_on_error(partial(processor.process_ticket, ticket))
+
+
 def main():
     alma = Alma()
     tracker = Tracker()
 
     for processor in get_processors(tracker, alma):
-        for ticket in processor.get_tickets():
-            while True:
-                try:
-                    processor.process_ticket(ticket)
-                    break
-                except requests.RequestException as ex:
-                    log.warning('[#%s] Got requestion exception: %s, will retry in a sec.', ticket['id'], ex)
-                    time.sleep(3)
-                    # retry
-                    pass
-                except rt.UnexpectedResponse as ex:
-                    log.warning('[#%s] Got unexpected response from RT: %s, will retry in a sec.', ticket['id'], ex)
-                    time.sleep(3)
-                    # retry
-                    pass
-                except rt.ConnectionError as ex:
-                    log.warning('[#%s] Connection error while processing: %s, will retry in a sec.', ticket['id'], ex)
-                    time.sleep(3)
-                    # retry
-                    pass
-            time.sleep(1)
+        retry_on_error(partial(process_tickets, processor))
 
 
 if __name__ == '__main__':
